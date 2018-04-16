d47a3556b8f21f34a4a833723e541808c3c81906
Kamran  Kowsari <kk7nc@virginia.edu>
2018-04-16 19:12:50 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -18,3 +18,61 @@ setup(
     long_description=read('README.md'),
     install_requires=required
 )
+
+
+import io
+import os
+
+
+import numpy
+from setuptools import Extension
+from setuptools import setup, find_packages
+
+__author__ = 'Kamran Kowsari <kowsari.net>'
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+
+def readfile(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    return io.open(path, encoding='utf8').read()
+
+
+setup(
+    name='RMDL',
+    version='1.0.0',
+    description='RMDL: Random Multimodel Deep Learning for Classification',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    url='https://github.com/kk7nc/RMDL',
+    author='Kamran Kowsari',
+    author_email='kk7nc@virginia.edu',
+    packages=['RMDL'] + find_packages('RMDL'),
+    install_requires=[
+        'matplotlib==2.1.2',
+        'numpy==1.12.1',
+        'pandas==0.22.0',
+        'scipy',
+        'tensorflow-gpu',
+        'keras==2.0.9',
+        'scikit-learn==0.19.0',
+        'nltk==3.2.4'
+    ],
+    include_package_data=False
+)
+
