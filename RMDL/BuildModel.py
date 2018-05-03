a6a26c0f4b9627e0c663cd463f530f3f070a2b15
Kamran  Kowsari <kk7nc@virginia.edu>
2018-05-03 15:47:10 -0400

    SGD is removed

--- RMDL/BuildModel.py
+++ RMDL/BuildModel.py
@@ -2,7 +2,7 @@
 RMDL: Random Multimodel Deep Learning for Classification
 
 * Copyright (C) 2018  Kamran Kowsari <kk7nc@virginia.edu>
- * Last Update: 04/25/2018
+ * Last Update: 05/03/2018
 * This file is part of  RMDL project, University of Virginia.
 * Free to use, change, share and distribute source code of RMDL
 * Refrenced paper : RMDL: Random Multimodel Deep Learning for Classification
