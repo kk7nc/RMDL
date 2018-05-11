ca186233bf32df3c5e3f651d0d7cbf588c725908
Kamran  Kowsari <kk7nc@virginia.edu>
2018-05-11 16:48:14 -0400

    MAX_NB_WORDS is added to tf-idf

--- RMDL/text_feature_extraction.py
+++ RMDL/text_feature_extraction.py
@@ -132,8 +132,9 @@ def loadData_Tokenizer(X_train, X_test,GloVe_DIR,MAX_NB_WORDS,MAX_SEQUENCE_LENGT
     print('Total %s word vectors.' % len(embeddings_index))
     return (X_train, X_test, word_index,embeddings_index)
 
-def loadData(X_train, X_test):
-    vectorizer_x = TfidfVectorizer()
+def loadData(X_train, X_test,MAX_NB_WORDS=75000):
+    vectorizer_x = TfidfVectorizer(max_features=MAX_NB_WORDS)
     X_train = vectorizer_x.fit_transform(X_train).toarray()
     X_test = vectorizer_x.transform(X_test).toarray()
+    print("tf-idf with",str(np.array(X_train).shape[1]),"features")
     return (X_train,X_test)
